c429b0e777ba2d6acbb47e44ca97167ac4967939
LyricLy <hanson.twentytwo@gmail.com>
2017-06-01 20:13:21 +1200

    use thecodindood's private message code

--- addons/troll.py
+++ addons/troll.py
@@ -8,13 +8,28 @@ class Troll:
         print('Addon "{}" loaded'.format(self.__class__.__name__))
         
     @commands.command()
-    async def pm(self, user, message):
-        """PM a specified user."""
+    async def pm(self, ctx, user, *, message=""):
+        """PM's a member since lyrics coding skills are too bad for this."""
         try:
-            user = get_member_named(user)
-            self.bot.send_message(user, message)
-        except Exception:
-            self.bot.say("Invalid user!")
+            subMsg = ctx.message.content.split(" ")[1]
+            memberName = subMsg.strip()
+            if memberName:
+                try:
+                    member = ctx.message.mentions[0]
+                except:
+                    member = ctx.message.server.get_member_named(memberName)
+                if not member:
+                    member = ctx.message.server.get_member(memberName)
+                if not member:
+                    await self.bot.send_message(ctx.message.channel, bot_prefix + 'Invalid user!')
+            msg_user = message
+            try:
+                await self.bot.send_message(member, msg_user)
+            except discord.errors.Forbidden: # if Goku is blocked
+                pass  
+            await self.bot.say("Successfully sent a message to " + user + "!")    
+        except discord.errors.Forbidden as e:
+            await self.bot.say("An error occurred! " + e)
             
 def setup(bot):
     bot.add_cog(Troll(bot))
