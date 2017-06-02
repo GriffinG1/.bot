1d823046b44ea3668a628bd1b0716d18d27a9468
LyricLy <hanson.twentytwo@gmail.com>
2017-06-02 12:54:30 +1200

    change success message

--- addons/troll.py
+++ addons/troll.py
@@ -10,25 +10,22 @@ class Troll:
     @commands.command(pass_context=True)
     async def pm(self, ctx, user, *, message=""):
         """Sends a PM to a specified member."""
-        try:
-            subMsg = ctx.message.content.split(" ")[1]
-            memberName = subMsg.strip()
-            if memberName:
-                try:
-                    member = ctx.message.mentions[0]
-                except:
-                    member = ctx.message.server.get_member_named(memberName)
-                if not member:
-                    member = ctx.message.server.get_member(memberName)
-                if not member:
-                    await self.bot.send_message(ctx.message.channel, 'Invalid user!')
+        subMsg = ctx.message.content.split(" ")[1]
+        memberName = subMsg.strip()
+        if memberName:
             try:
-                await self.bot.send_message(member, message)
-            except discord.errors.Forbidden: # if Goku is blocked
-                await self.bot.say("Could not send message. The user likely has the bot blocked.")  
-            await self.bot.say("Successfully sent a message to " + user + "!")    
-        except discord.errors.Forbidden as e:
-            await self.bot.say("An error occurred! " + e)
+                member = ctx.message.mentions[0]
+            except:
+                member = ctx.message.server.get_member_named(memberName)
+            if not member:
+                member = ctx.message.server.get_member(memberName)
+            if not member:
+                await self.bot.send_message(ctx.message.channel, 'Invalid user!')
+        try:
+            await self.bot.send_message(member, message)
+            await self.bot.say("Successfully sent a message to {}#{}!".format(member.name, member.discriminator))    
+        except discord.errors.Forbidden: # if Goku is blocked
+            await self.bot.say("Could not send message. The user likely has the bot blocked.")  
             
 def setup(bot):
     bot.add_cog(Troll(bot))
