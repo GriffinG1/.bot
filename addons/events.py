3f53cfeb85993ad5eb619f1c80bed0edaddee1fb
LyricLy <hanson.twentytwo@gmail.com>
2017-06-20 10:58:20 +1200

    fix logging and role-adding bypass by disabling DMs on join

--- addons/events.py
+++ addons/events.py
@@ -7,16 +7,16 @@ git = git.cmd.Git(".")
 class Events:
     """Event handling."""
 
-    def __init__(self, bot):   
+    def __init__(self, bot):
         self.bot = bot
         print('Addon "{}" loaded'.format(self.__class__.__name__))
-    
+
     async def on_message(self, message):
         #filter "it seem"                                                  #thecommondude
         if message.content.startswith("it seem ") and message.author.id == "135204578986557440":
             await self.bot.send_message(message.channel, "STFU dude.")
             await self.bot.delete_message(message)
-        #auto update 
+        #auto update
         if message.author.name == "GitHub":
             print("Pulling changes!")
             git.pull()
@@ -27,18 +27,21 @@ class Events:
             await self.bot.send_message(self.bot.private_messages_channel, "Private message sent by {}#{}:".format(message.author.name, message.author.discriminator), embed=embed)
 
     async def on_member_join(self, member):
-        await self.bot.send_message(member, "Welcome to the official Nintendo Homebrew Idiot Log server! Please read our {} and have a ~~horrible~~ great time!".format(self.bot.rules_channel.mention))
+        try:
+            await self.bot.send_message(member, "Welcome to the official Nintendo Homebrew Idiot Log server! Please read our {} and have a ~~horrible~~ great time!".format(self.bot.rules_channel.mention))
+        except discord.errors.Forbidden: # doesn't accept DMs from non-friends
+            pass
         await self.bot.add_roles(member, self.bot.idiots_role)
         await self.bot.send_message(self.bot.logs_channel, ":exclamation: Member joined: {}#{}".format(member.name, member.discriminator))
-        
+
     async def on_member_leave(self, member):
         await self.bot.send_message(self.bot.logs_channel, ":exclamation: Member left: {}#{}".format(member.name, member.discriminator))
-        
+
     async def on_member_ban(self, member):
         await self.bot.send_message(self.bot.logs_channel, ":exclamation: Member banned: {}#{}".format(member.name, member.discriminator))
-        
+
     async def on_member_unban(self, server, member):
         await self.bot.send_message(self.bot.logs_channel, ":exclamation: Member unbanned: {}#{}".format(member.name, member.discriminator))
-        
+
 def setup(bot):
     bot.add_cog(Events(bot))
