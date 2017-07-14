86877da3b6a3fd3e774158bc11053f116b58b879
GriffinG1 <griffin.giargiari@gmail.com>
2017-07-14 15:07:58 -0700

    Removed ability to @everyone with .say

--- addons/troll.py
+++ addons/troll.py
@@ -32,6 +32,7 @@ class Troll:
     @commands.command(pass_context=True)
     async def say(self, ctx, channel, *, msg):
         """Says a message in a specified channel."""
+        msg = msg.replace("@everyone", "`@`everyone")
         await self.bot.send_message(ctx.message.channel_mentions[0], msg)
         
 def setup(bot):
