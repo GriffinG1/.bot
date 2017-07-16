796dbd2dfcf97f2840d0ee047e36e33535a6f260
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-07-15 21:34:04 -0700

    maybe?

--- addons/mod.py
+++ addons/mod.py
@@ -110,7 +110,7 @@ class Moderation:
         else:
             owner = server.owner
             
-            if (self.bot.server_admin_role in found_member.roles) and not (found_member = owner):
+            if (not found_member = owner) and (self.bot.server_admin_role in found_member.roles):
                 return await self.bot.say("You cannot warn a staff member!")
             if not 
             try:
