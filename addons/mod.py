8b62d479e005245b360a3ec117f675b349ba2ae2
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-07-15 21:31:09 -0700

    owner bypass warn

--- addons/mod.py
+++ addons/mod.py
@@ -108,8 +108,11 @@ class Moderation:
         if not found_member:
             await self.bot.say("That user could not be found.")
         else:
-            if self.bot.server_admin_role in found_member.roles:
+            owner = server.owner
+            
+            if (self.bot.server_admin_role in found_member.roles) and not (found_member = owner):
                 return await self.bot.say("You cannot warn a staff member!")
+            if not 
             try:
                 self.warns[found_member.id]
             except KeyError:
