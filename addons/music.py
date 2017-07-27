adc4be29ae393ae9a7cbfe6b5c5042395e1decef
Griffin <4n0nym0ush4ck3r666@gmail.com>
2017-07-26 17:36:05 -0700

    tests

--- addons/music.py
+++ addons/music.py
@@ -43,7 +43,7 @@ class Music:
         if self.voice_channel:
             self.player = await self.voice_channel.create_ytdl_player(url)
             self.player.start()
-            await self.bot.say("Now playing:" + url)
+            await self.bot.say("Now playing: " + player.title)
         else:
             await self.bot.say("How am I supposed to play music when I'm not in a voice channel? *Think. Think. Think.* Please.")
             
