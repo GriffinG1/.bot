53180593653feaa7f77a19322962a1d4d75204f1
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-09-11 17:45:13 -0700

    Alias for bot use

--- addons/rules.py
+++ addons/rules.py
@@ -45,7 +45,7 @@ class Rules:
     async def nickname(self):
         await self.simple_embed("If you would like a nickname, please DM it to @FuckFace McGee, and your nickname will be applied ASAP!")
         
-    @commands.command(pass_context=True, hidden=True)
+    @commands.command(pass_context=True, hidden=True, aliases=['bu'])
     async def botuse(self):
         await self.simple_embed("**By participating in this server, you acknowledge that user data (including messages, user IDs, user tags) will be collected and logged for moderation purposes. If you disagree with this collection, please leave the server immediately.**")
 
