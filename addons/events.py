316959ec337c5ff26d8d1d0b38087da34dc39534
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-09-17 21:26:09 -0700

    And blacklist checking

--- addons/events.py
+++ addons/events.py
@@ -71,6 +71,9 @@ class Events:
             pass
         await self.bot.add_roles(member, self.bot.idiots_role)
         embed = discord.Embed(title=":wave: Member joined", description="<@{}> | {}#{} | {}".format(member.id, member.name, member.discriminator, member.id))
+        async for message in self.bot.blacklist_channel.history():
+            if member.mention in message.content:
+                embed.set_footer(text="This user is blacklisted.")
         await self.bot.send_message(self.bot.logs_channel, ":exclamation:", embed=embed)
 
     async def on_member_remove(self, member):
