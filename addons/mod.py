432db46edeb87c9edbd09c07b70dba6a9b9de4e1
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-10-02 16:27:48 -0700

    Partial rewrite to mod

--- addons/mod.py
+++ addons/mod.py
@@ -9,9 +9,6 @@ class Moderation:
     """Bot commands for moderation."""
     def __init__(self, bot):
         self.bot = bot
-        with open('saves/warns.json', 'r+') as f:
-            self.warns = json.load(f)
-        print('Addon "{}" loaded'.format(self.__class__.__name__))
         
     def find_user(self, user, ctx):
         found_member = self.bot.server.get_member(user)
@@ -33,21 +30,21 @@ class Moderation:
         """Kick a member."""
         found_member = self.find_user(member, ctx)
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
             if reason:
                 reason_msg = "The given reason was: {}".format(reason)
             else:
                 reason_msg = "No reason was given."
             try:
-                await self.bot.send_message(found_member, "You have been kicked by user {0.name}#{0.discriminator}.\n{2}\nYou can rejoin the server with this link: https://discord.gg/hHHKPFz".format(ctx.message.author, self.bot.rules_channel.mention, reason_msg))
+                await found_member.send("You have been kicked by user {0.name}#{0.discriminator}.\n{2}\nYou can rejoin the server with this link: https://discord.gg/hHHKPFz".format(ctx.message.author, self.bot.rules_channel.mention, reason_msg))
             except discord.errors.Forbidden:
                 pass
-            await self.bot.kick(found_member)
-            await self.bot.say("Successfully kicked user {0.name}#{0.discriminator}!".format(found_member))
+            await found_member.kick()
+            await ctx.send("Successfully kicked user {0.name}#{0.discriminator}!".format(found_member))
             embed = discord.Embed(description="<@{0.id}> | {0.name}#{0.discriminator} kicked user <@{1.id}> | {1.name}#{1.discriminator}".format(ctx.message.author, found_member))
             embed.add_field(name="Reason given", value="• " + reason)
-            await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+            await self.bot.cmd_logs_channel(embed=embed)
     
     @commands.has_permissions(ban_members=True)    
     @commands.command(pass_context=True)
@@ -55,22 +52,21 @@ class Moderation:
         """Ban a member."""
         found_member = self.find_user(member, ctx)
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
             if reason:
                 reason_msg = "The given reason was: {}".format(reason)
             else:
                 reason_msg = "No reason was given."
-            await self.bot.send_message(found_member, "You have been banned by user {0.name}#{0.discriminator}.\n{2}\nIf you feel that you did not deserve this ban, send a direct message to one of the staff on the Server Admins list in {1}.\nIn the rare scenario that you do not have the entire staff list memorized, LyricLy#5752 and Griffin#2329 are two choices you could use.".format(ctx.message.author, self.bot.rules_channel.mention, reason_msg))
             try: 
-                await self.bot.send_message(found_member, "You have been banned by user {0.name}#{0.discriminator}.\n{2}\nIf you feel that you did not deserve this ban, send a direct message to one of the staff on the Server Admins list in {1}.\nIn the rare scenario that you do not have the entire staff list memorized, YourLocalLyric#5752 and Griffin#2329 are two choices you could use.".format(ctx.message.author, self.bot.rules_channel.mention, reason_msg))
+                await found_member.send("You have been banned by user {0.name}#{0.discriminator}.\n{2}\nIf you feel that you did not deserve this ban, send a direct message to one of the Server Admins.\nIn the rare scenario that you do not have the entire staff list memorized, you can DM <@177939404243992578> | Griffin#2329.".format(ctx.message.author, self.bot.rules_channel.mention, reason_msg))
             except discord.errors.Forbidden:
                 pass
-            await self.bot.ban(found_member, 0)
-            await self.bot.say("Successfully banned user {0.name}#{0.discriminator}!".format(found_member))
+            await found_member.ban(0)
+            await ctx.send("Successfully banned user {0.name}#{0.discriminator}!".format(found_member))
             embed = discord.Embed(description="<@{0.id}> | {0.name}#{0.discriminator} banned user <@{1.id}> | {1.name}#{1.discriminator}".format(ctx.message.author, found_member))
             embed.add_field(name="Reason given", value="• " + reason)
-            await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+            await self.bot.cmd_logs_channel.send(embed=embed)
             
     @commands.has_permissions(ban_members=True)    
     @commands.command(pass_context=True)
@@ -78,19 +74,19 @@ class Moderation:
         """Mute a member."""
         found_member = self.find_user(member, ctx)
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
-            await self.bot.add_roles(found_member, self.bot.muted_role)
+            await found_member.add_roles(self.bot.muted_role)
             if reason:
                 reason_msg = "The given reason was: {}".format(reason)
             else:
                 reason_msg = "No reason was given."
-            await self.bot.say("Successfully muted user {0.name}#{0.discriminator}!".format(found_member))
+            await ctx.send("Successfully muted user {0.name}#{0.discriminator}!".format(found_member))
             embed = discord.Embed(description="<@{0.id}> | {0.name}#{0.discriminator} muted user <@{1.id}> | {1.name}#{1.discriminator}".format(ctx.message.author, found_member))
             embed.add_field(name="Reason given", value="• " + reason)
-            await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+            await self.bot.cmd_logs_channel.send(embed=embed)
             try:
-                await self.bot.send_message(found_member, "You have been muted by user {0.name}#{0.discriminator}.\n{2}\nIf you feel that you did not deserve this mute, send a direct message to one of the staff on the Server Admins list in {1}.".format(ctx.message.author, self.bot.rules_channel.mention, reason_msg))
+                await found_member.send("You have been muted by user {0.name}#{0.discriminator}.\n{2}\nIf you feel that you did not deserve this mute, send a direct message to one of the staff on the Server Admins list in {1}.".format(ctx.message.author, self.bot.rules_channel.mention, reason_msg))
             except discord.errors.Forbidden:
                 pass
             
@@ -100,32 +96,32 @@ class Moderation:
         """Unmute a member."""
         found_member = self.find_user(member, ctx)
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
             if self.bot.muted_role in found_member.roles:
-                await self.bot.remove_roles(found_member, self.bot.muted_role)
+                await found_member.remove_roles(self.bot.muted_role)
                 embed = discord.Embed(description="{0.name}#{0.discriminator} unmuted user <@{1.id}> | {1.name}#{1.discriminator}".format(ctx.message.author, found_member))
-                await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
-                await self.bot.send_message(found_member, "You have been unmuted by user {0.name}#{0.discriminator}. Don't do it again!".format(ctx.message.author))
-                await self.bot.say("Successfully unmuted user {0.name}#{0.discriminator}!".format(found_member))
+                await self.bot.cmd_logs_channel.send(embed=embed)
+                await found_member.send("You have been unmuted by user {0.name}#{0.discriminator}. Don't do it again!".format(ctx.message.author))
+                await ctx.send("Successfully unmuted user {0.name}#{0.discriminator}!".format(found_member))
             else:
-                await self.bot.say("That user isn't muted!")
+                await ctx.send("That user isn't muted!")
 
     @commands.has_permissions(kick_members=True)
     @commands.command(pass_context=True)
     async def uncontain(self, ctx, member):
         """Remove a member from #containment."""
-        await self.bot.delete_message(ctx.message)
+        ctx.message.delete()
         found_member = self.find_user(member, ctx)
         member_roles = found_member.roles
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
-            await self.bot.replace_roles(found_member, self.bot.unhelpful_jerks_role)
+            await found_member.edit(roles=self.bot.unhelpful_jerks_role)
             embed = discord.Embed(description="{0.name}#{0.discriminator} moved user <@{1.id}> | {1.name}#{1.discriminator} out of <#335599294553915392>".format(ctx.message.author, found_member))
-            await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+            await self.bot.cmd_logs_channel(embed=embed)
             try:
-                await self.bot.send_message(found_member, "Enjoy the server!")
+                await found_member.send("Enjoy the server!")
             except discord.errors.Forbidden:
                 pass
             
@@ -134,28 +130,25 @@ class Moderation:
     async def promote(self, ctx):
         """Upgrade Roles"""
         if ctx.invoked_subcommand is None:
-            message = await self.bot.say("You're missing a parameter!")
-            await self.bot.delete_message(ctx.message)
-            await asyncio.sleep(3)
-            await self.bot.delete_message(message)
+            await ctx.message.delete()
+            await ctx.send("You're missing a parameter!", delete_after=3)
 
     @commands.has_permissions(kick_members=True)
     @promote.command(pass_context=True)
     async def Neutron(self, ctx, *, member):
         """Neutron Stars"""
-        await self.bot.delete_message(ctx.message)
+        ctx.message.delete()
         found_member = self.find_user(member, ctx)
         member_roles = found_member.roles
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
-            # await self.bot.replace_roles(found_member, self.bot.neutron_stars_role
-            await self.bot.add_roles(found_member, self.bot.neutron_stars_role)
-            await self.bot.remove_roles(found_member, self.bot.unhelpful_jerks_role)
+            await found_member.add_roles(self.bot.neutron_stars_role)
+            await found_member.remove_roles(self.bot.unhelpful_jerks_role)
             embed = discord.Embed(description="{0.name}#{0.discriminator} promoted user <@{1.id}> | {1.name}#{1.discriminator} to Neutron Stars!".format(ctx.message.author, found_member))
-            await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+            await self.bot.cmd_logs_channel.send(embed=embed)
             try:
-                await self.bot.send_message(found_member, "You're a Neutron Star now! Congrats, you're the densest known thing in the universe!")
+                await found_member.send("You're a Neutron Star now! Congrats, you're the densest known thing in the universe!")
             except discord.errors.Forbidden:
                 pass
 
@@ -163,17 +156,17 @@ class Moderation:
     @promote.command(pass_context=True)
     async def Sheet(self, ctx, *, member):
         """Sheet Admins"""
-        await self.bot.delete_message(ctx.message)
+        ctx.message.delete()
         found_member = self.find_user(member, ctx)
         member_roles = found_member.roles
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
-            await self.bot.add_roles(found_member, self.bot.sheet_admin_role)
+            await found_member.add_roles(self.bot.sheet_admin_role)
             embed = discord.Embed(description="{0.name}#{0.discriminator} promoted user <@{1.id}> | {1.name}#{1.discriminator} to Sheet Admin!".format(ctx.message.author, found_member))
-            await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+            await self.bot.cmd_logs_channel.send(embed=embed)
             try:
-                await self.bot.send_message(found_member, "You're a Sheet Admin now!")
+                await found_member.send("You're a Sheet Admin now!")
             except discord.errors.Forbidden:
                 pass
 
@@ -181,17 +174,17 @@ class Moderation:
     @promote.command(pass_context=True)
     async def Server(self, ctx, *, member):
         """Server Admins"""
-        await self.bot.delete_message(ctx.message)
+        await ctx.message.delete()
         found_member = self.find_user(member, ctx)
         member_roles = found_member.roles
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            ctx.send("That user could not be found.")
         else:
-            await self.bot.add_roles(found_member, self.bot.server_admin_role)
+            await found_member.add_roles(self.bot.server_admin_role)
             embed = discord.Embed(description="{0.name}#{0.discriminator} promoted user <@{1.id}> | {1.name}#{1.discriminator} to Server Admin!".format(ctx.message.author, found_member))
-            await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+            await self.bot.cmd_logs_channel.send(embed=embed)
             try:
-                await self.bot.send_message(found_member, "You're a Server Admin now!")
+                await found_member.send("You're a Server Admin now!")
             except discord.errors.Forbidden:
                 pass
     
@@ -199,49 +192,44 @@ class Moderation:
     @commands.command(pass_context=True)
     async def nosupport(self, ctx, member, *, reason="No reason given."):
         """Kicks a user out of support"""
-        await self.bot.delete_message(ctx.message)
+        await ctx.message.delete()
         found_member = self.find_user(member, ctx)
-        channel = self.bot.server.get_channel("336761748159987713")
+        channel = self.bot.server.get_channel(336761748159987713)
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
             if self.bot.support_role in found_member.roles:
-                await self.bot.remove_roles(found_member, self.bot.support_role)
-                overwrites = channel.overwrites_for(found_member)
-                overwrites.read_messages = False
-                await self.bot.edit_channel_permissions(channel, found_member, overwrites)
+                await found_member.remove_roles(self.bot.support_role)
+                await channel.set_permissions(found_member, read_messages=False)
                 embed = discord.Embed(description="{0.name}#{0.discriminator} kicked user <@{1.id}> | {1.name}#{1.discriminator} from <#336761748159987713>".format(ctx.message.author, found_member))
                 embed.add_field(name="Reason given", value="• " + reason)
-                await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+                await self.bot.cmd_logs_channel.send(embed=embed)
                 try:
-                    await self.bot.send_message(found_member, "You were removed from 9-1-1-tech-support by user {}#{}. The given reason was: `{}` \nIf you feel that you did not deserve this removal, send a direct message to one of the staff on the Server Admins list in <#318626746297745409>".format(ctx.message.author.name, ctx.message.author.discriminator, reason))
+                    await found_member.send("You were removed from 9-1-1-tech-support by user {}#{}. The given reason was: `{}` \nIf you feel that you did not deserve this removal, send a direct message to one of the staff on the Server Admins list in <#318626746297745409>".format(ctx.message.author.name, ctx.message.author.discriminator, reason))
                 except discord.errors.Forbidden:
                     pass
-                await self.bot.say("{}#{} can no longer access support.".format(found_member.name, found_member.discriminator))
+                await ctx.send("{}#{} can no longer access support.".format(found_member.name, found_member.discriminator))
             else:
-                await self.bot.say("That user isn't in support!")
+                await ctx.send("That user isn't in support!")
                 
     @commands.has_permissions(kick_members=True)
     @commands.command(pass_context=True)
     async def givesupport(self, ctx, member):
         """Allows a user to rejoin support"""
-        await self.bot.delete_message(ctx.message)
+        await ctx.message.delete()
         found_member = self.find_user(member, ctx)
-        channel = self.bot.server.get_channel("336761748159987713")
+        channel = self.bot.server.get_channel(336761748159987713)
         if not found_member:
-            await self.bot.say("That user could not be found.")
+            await ctx.send("That user could not be found.")
         else:
-            await self.bot.add_roles(found_member, self.bot.support_role)
-            overwrites = channel.overwrites_for(found_member)
-            overwrites.read_messages = True
-            await self.bot.edit_channel_permissions(channel, found_member, overwrites)
+            await channel.set_permissions(found_member, read_messages=True)
             embed = discord.Embed(description="{0.name}#{0.discriminator} restored user <@{1.id}> | {1.name}#{1.discriminator} permissions for <#336761748159987713>".format(ctx.message.author, found_member))
-            await self.bot.send_message(self.bot.cmd_logs_channel, embed=embed)
+            await self.bot.cmd_logs_channel.send(embed=embed)
             try:
-                await self.bot.send_message(found_member, "You can access <#336761748159987713> again!")
+                await found_member.send("You can access <#336761748159987713> again!")
             except discord.errors.Forbidden:
                 pass
-            await self.bot.say("{}#{} can access support again.".format(found_member.name, found_member.discriminator))
+            await ctx.send("{}#{} can access support again.".format(found_member.name, found_member.discriminator))
             
 def setup(bot):
     bot.add_cog(Moderation(bot))
