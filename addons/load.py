e22db4731a9bbb7836fab39cb794c32cce642697
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-10-04 16:04:15 -0700

    Rewrite reload, hopefully
    Idk I'm just using my loadall code, slightly modified

--- addons/load.py
+++ addons/load.py
@@ -38,17 +38,22 @@ class Load:
             await ctx.send(':white_check_mark: Extension unloaded.')
    
     @commands.has_permissions(ban_members=True)
-    @commands.command(name='reload', hidden=True)
-    async def _reload(self, *, module : str):
+    @commands.command(hidden=True)
+    async def reload(self, ctx):
         """Reloads an addon."""
-        try:
-            if module[0:7] != "addons.":
-                module = "addons." + module
-            self.bot.unload_extension(module)
-            self.bot.load_extension(module)
-            await ctx.send(':white_check_mark: Extensions reloaded.')
-        except Exception as e:
-            await ctx.send(':anger: Failed!\n```\n{}: {}\n```'.format(type(e).__name__, e))
+        errors = ""
+        for addon in os.listdir("addons"):
+            if ".py" in addon:
+                cog = cog.replace('.py', '')
+                try:
+                    self.bot.unload_extension("cogs.{}".format(addon))
+                    self.bot.load_extension("cogs.{}".format(addon))
+                except Exception as e:
+                    errors += 'Failed to load module: `{}.py` due to `{}: {}`\n'.format(addon, type(e).__name__, e)
+        if not errors:
+            await ctx.send(self.bot.bot_prefix + "All addons reloaded")
+        else:
+            await ctx.send(self.bot.bot_prefix + errors)
 
 def setup(bot):
     bot.add_cog(Load(bot))
