7c085b45a9d91ee81930dc32133ee134892bd573
Griffin <4n0nym0ush4ck3r666@gmail.com>
2017-10-03 23:21:00 -0700

    Start load rewrite

--- addons/load.py
+++ addons/load.py
@@ -14,31 +14,29 @@ class Load:
     # Load test
     @commands.has_permissions(ban_members=True)
     @commands.command(hidden=True)
-    async def load(self, *, module : str):
-        """Loads an addon."""
+    async def load(self, ctx, *, module):
+        """Load an addon"""
         try:
-            if module[0:7] != "addons.":
-                module = "addons." + module
-            self.bot.load_extension(module)
-            await ctx.send(':white_check_mark: Extension loaded.')
+            self.bot.load_extension("addons.{}".format(module))
         except Exception as e:
             await ctx.send(':anger: Failed!\n```\n{}: {}\n```'.format(type(e).__name__, e))
+        else:
+            await ctx.send(':white_check_mark: Extension loaded.')
 
     @commands.has_permissions(ban_members=True)
     @commands.command(hidden=True)
-    async def unload(self, *, module : str):
-        """Unloads an addon."""
+    async def unload(self, ctx, *, module):
+        """Unload a module"""
         try:
-            if module[0:7] != "addons.":
-                module = "addons." + module
             if module == "addons.load":
                 await ctx.send(":exclamation: I don't think you want to unload that!")
             else:
-                self.bot.unload_extension(module)
-                await ctx.send(':white_check_mark: Extension unloaded.')
+            self.bot.unload_extension("addons.{}".format(module))
         except Exception as e:
             await ctx.send(':anger: Failed!\n```\n{}: {}\n```'.format(type(e).__name__, e))
-
+        else:
+            await ctx.send(':white_check_mark: Extension unloaded.')
+   
     @commands.has_permissions(ban_members=True)
     @commands.command(name='reload', hidden=True)
     async def _reload(self, *, module : str):
@@ -48,7 +46,7 @@ class Load:
                 module = "addons." + module
             self.bot.unload_extension(module)
             self.bot.load_extension(module)
-            await ctx.send(':white_check_mark: Extension reloaded.')
+            await ctx.send(':white_check_mark: Extensions reloaded.')
         except Exception as e:
             await ctx.send(':anger: Failed!\n```\n{}: {}\n```'.format(type(e).__name__, e))
 
