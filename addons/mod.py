0a3738517ddaface6a904d0032ab8bba36f08916
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-10-09 14:03:23 -0700

    When will my code come back from the war

--- addons/mod.py
+++ addons/mod.py
@@ -145,8 +145,8 @@ class Moderation:
             await ctx.send("That user could not be found.")
         else:
             for role in member_roles:
-                await found_member.remove_roles(role)
                 rolelist += "•{}\n".format(role)
+            found_member.edit(roles=[])
             await found_member.add_roles(self.bot.idiots_role)
             embed = discord.Embed(description="{}#{} recontained user {} | {}#{} for \n•{}. Users roles were:\n{}".format(ctx.message.author.name, ctx.message.author.discriminator, found_member.mention, found_member.name, found_member.discriminator, reason, rolelist))
             await self.bot.cmd_logs_channel.send(embed=embed)
