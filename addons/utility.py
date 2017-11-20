ceac7f72aa49a7a99248b148c87736325c0138be
Griffin <4n0nym0ush4ck3r666@gmail.com>
2017-11-19 20:44:32 -0800

    Change test command

--- addons/utility.py
+++ addons/utility.py
@@ -14,6 +14,9 @@ class Utility:
         """A test command."""
         embed = discord.Embed(title="testing", description="Testing")
         embed.add_field(name="Notes", value="Testing!", inline=False)
+        for x in self.bot.guilds:
+            servers += x + "\n"
+        embed.add_field(name="Notes", value=servers, inline=False)
         embed.colour = discord.Colour(0x00FFFF)            
         await ctx.send(embed=embed)
 
